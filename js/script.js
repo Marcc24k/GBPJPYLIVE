2dc668c9ae07f0fa987323decf662ba335fbd181
Marc <marcc24k@gmail.com>
2025-02-24 16:02:06 -0500

    Modified session time to reflect opena close time:

--- js/script.js
+++ js/script.js
@@ -88,13 +88,11 @@ function updateTime() {
 
     // Change text color based on session start time
     if (gmt >= sessionStart && gmt > sessionClose) {
-        // sessionStartElement.style.color = "green"; // Session is active
-        sessionCloseElement.style.color = "green"; // Display session close time in dark red
-        sessionStartElement.style.display = "none"; // Display session close time in dark red
+        sessionStartElement.style.color = "#982dc9"; // Session is active
+        sessionCloseElement.style.display = "none"; // Display session close time in dark red
     } else {
-        sessionStartElement.style.color = "green"; // Session is inactive
-        sessionCloseElement.style.display = "none"; // Session is inactive
-        // sessionCloseElement.style.color = "green"; // Session close time in green
+        sessionStartElement.style.color = "#982dc9"; // Session is inactive
+        sessionCloseElement.style.display = "none"; // Session close time in green
     }
 
     marketStatusElement.textContent = marketStatus; // Display the market status
